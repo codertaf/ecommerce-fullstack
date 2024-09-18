ae37bb235e214d6d78ad93b551fab2bd41525734
codertaf <yordytp10@gmail.com>
2024-09-18 13:56:42 +0200

    New Dto

--- Backend/src/main/java/com/erp/ecommerce/dto/CategoryDto.java
+++ Backend/src/main/java/com/erp/ecommerce/dto/CategoryDto.java
@@ -1,4 +1,27 @@
 package com.erp.ecommerce.dto;
 
+import com.erp.ecommerce.entity.Product;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@NoArgsConstructor
+@AllArgsConstructor
 public class CategoryDto {
+
+    private Long id;
+    private String name;
+    private List<Product> productList;
+
+    private  LocalDateTime createdAt;
 }
+
