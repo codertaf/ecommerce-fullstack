7e7d8038050bec39c711beef1ceb90f00f6be62a
codertaf <yordytp10@gmail.com>
2024-09-23 20:45:38 +0200

    Exceptions finished

---
+++ Backend/src/main/java/com/erp/ecommerce/exception/InvalidCredentialExceptions.java
@@ -0,0 +1,7 @@
+package com.erp.ecommerce.exception;
+
+public class InvalidCredentialExceptions extends RuntimeException {
+    public InvalidCredentialExceptions(String message){
+        super(message);
+    }
+}
